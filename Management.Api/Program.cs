cd451aff1a9917253212dbdbcba82041ae504c18
Andreea Mihalcea <andreea.mihalcea@tremend.com>
2023-01-21 14:57:40 +0200

    wip refactoring

--- Management.Api/Program.cs
+++ Management.Api/Program.cs
@@ -1,3 +1,5 @@
+using Management.Domain.Clients;
+using Management.Infrastructure.Domain.Clients;
 using Management.Models;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
@@ -9,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<ClientConfiguration>(builder.Configuration.GetSection(nameof(ClientConfiguration)));
 builder.Services.AddSingleton<ClientConfiguration>(sp => sp.GetRequiredService<IOptions<ClientConfiguration>>().Value);
 builder.Services.AddSingleton<IMongoClient>(s => new MongoClient(builder.Configuration.GetValue<string>("ManagementDatabaseSettings:ConnectionString")));
-//builder.Services.AddScoped<IClientCommandRepository, ClientCommandRepository>();
+builder.Services.AddScoped<IClientCommandRepository, ClientCommandRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
